8e52813b102ae17ee563fcea8780246f4e4b14a7
alexwololo <alexander.caspersson@gmail.com>
2022-06-10 23:47:19 +0200

    List building

---
+++ ReactNative/rn-starter/src/screens/ListScreen.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
+import {
+  borderBottomColor,
+  borderColor,
+} from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes';
+
+const ListScreen = () => {
+  const friends = [
+    { name: 'Friend #1', age: 20 },
+    { name: 'Friend #2', age: 45 },
+    { name: 'Friend #3', age: 32 },
+    { name: 'Friend #4', age: 27 },
+    { name: 'Friend #5', age: 53 },
+    { name: 'Friend #6', age: 30 },
+    { name: 'Friend #7', age: 38 },
+    { name: 'Friend #8', age: 18 },
+    { name: 'Friend #9', age: 16 },
+  ];
+
+  return (
+    <FlatList
+      vertical
+      showsVerticalScrollIndicator={false}
+      keyExtractor={friend => friend.name}
+      data={friends}
+      renderItem={({ item }) => {
+        return (
+          <Text style={styles.textStyle}>
+            {item.name} - Age {item.age}
+          </Text>
+        );
+      }}
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  textStyle: {
+    marginVertical: 50,
+  },
+});
+
+export default ListScreen;
