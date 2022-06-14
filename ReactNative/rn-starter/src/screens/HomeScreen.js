12f368c2a4beda7a4d7df38ae8d732fa0de2b279
alexwololo <alexander.caspersson@gmail.com>
2022-06-14 23:46:45 +0200

    change homescreen

--- ReactNative/rn-starter/src/screens/HomeScreen.js
+++ ReactNative/rn-starter/src/screens/HomeScreen.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import { Text, StyleSheet, View, Button } from 'react-native';
+import { Text, StyleSheet, View, Button, TouchableOpacity } from 'react-native';
 
 const HomeScreen = () => {
   return (
     <View>
       <Text style={styles.text}>HomeScreen</Text>
-      <Button title="Go to Components Demo" />
+      <Button
+        onPress={() => console.log('Button pressed')}
+        title="Go to Components Demo"
+      />
+      <TouchableOpacity onPress={() => console.log('list press')}>
+        <Text>Go to list demo</Text>
+      </TouchableOpacity>
     </View>
   );
 };
