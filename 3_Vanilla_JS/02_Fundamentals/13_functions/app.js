16a24a6992cf1b4d4954b01fd8666e5f8c76092d
alexwololo <alexander.caspersson@gmail.com>
2022-08-11 02:32:27 +0200

    update functions

--- 3_Vanilla_JS/02_Fundamentals/13_functions/app.js
+++ 3_Vanilla_JS/02_Fundamentals/13_functions/app.js
@@ -1,31 +1,6 @@
-// FUNCTION DECLARATIONS
 
-function greet(firstName = 'John', lastName = 'Doe'){
-  // if(typeof firstName === 'undefined'){firstName = 'John'}
-  // if(typeof lastName === 'undefined'){lastName = 'Doe'}
-  //console.log('Hello');
-  return 'Hello ' + firstName + ' ' + lastName;
-}
 
-// console.log(greet());
 
-// FUNCTION EXPRESIONS
-
-const square = function(x = 3){
-  return x*x;
-};
-
-// console.log(square());
-
-// IMMIDIATLEY INVOKABLE FUNCTION EXPRESSIONS - IIFEs
-
-// (function(){
-//   console.log('IIFE Ran..');
-// })();
-
-// (function(name){
-//   console.log('Hello '+ name);
-// })('Brad');
 
 // PROPERTY METHODS
 
