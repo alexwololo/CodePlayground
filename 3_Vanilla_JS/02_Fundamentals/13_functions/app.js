4bb07d116d276fe879d615fb34359cc60db532a2
alexwololo <alexander.caspersson@gmail.com>
2022-08-11 23:22:18 +0200

    function update

--- 3_Vanilla_JS/02_Fundamentals/13_functions/app.js
+++ 3_Vanilla_JS/02_Fundamentals/13_functions/app.js
@@ -1,22 +1,43 @@
-
-
-
-
 // PROPERTY METHODS
 
 const todo = {
-  add: function(){
+  add: function () {
     console.log('Add todo..');
   },
-  edit: function(id){
+  edit: function (id) {
     console.log(`Edit todo ${id}`);
-  }
-}
+  },
+};
 
-todo.delete = function(){
+todo.delete = function () {
   console.log('Delete todo...');
-}
+};
 
 todo.add();
 todo.edit(22);
 todo.delete();
+
+// call back and foreach
+
+const myFunc = (callbackFunc) => {
+  //do something
+  let value = 50;
+  callbackFunc(value);
+};
+
+myFunc(value => {
+  //do something
+  console.log(value);
+});
+
+let people = ['mario', 'luigi', 'ryu'];
+
+const logPerson = (person, index) => {
+  console.log(`${index} - heelo ${person}`);
+}
+
+people.forEach(function(person, index){
+  console.log(person, index);
+})
+
+people.forEach(logPerson);
