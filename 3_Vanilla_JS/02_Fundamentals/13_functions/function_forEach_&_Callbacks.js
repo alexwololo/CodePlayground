3bfa724ea1dae8e5f0f0ffddc84414ca4fb2870b
alexwololo <alexander.caspersson@gmail.com>
2024-07-30 23:52:28 +0200

    förklaring

--- 3_Vanilla_JS/02_Fundamentals/13_functions/function_forEach_&_Callbacks.js
+++ 3_Vanilla_JS/02_Fundamentals/13_functions/function_forEach_&_Callbacks.js
@@ -1,3 +1,16 @@
+/*
+Detta handlar om callbacks och forEach i JavaScript.
+
+En callback-funktion är en funktion som skickas som argument till en annan funktion:
+
+Exempel: myFunc tar en callback och anropar den med ett värde.
+Exempel med arrow function: myFunc((value) => console.log(value));
+forEach används för att iterera över en array:
+
+Exempel: people.forEach loggar varje person och deras index.
+Konverterat till arrow function: const logPerson = (person, index) => console.log(\${index} - hello ${person}`);och sedan användspeople.forEach(logPerson);`.
+*/
+
 //callbacks and forEach.
 //Callback function = when we pass a function as a argument
 
